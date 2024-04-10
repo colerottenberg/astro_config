bb9c0621673fac5bc9421de1d7a831cd2353fe36
Cole Rottenberg <cole.rottenberg@gmail.com>
2024-04-10 11:07:20 -0400

    working config... not perfect

---
+++ lua/plugins/copilot.lua
@@ -0,0 +1,13 @@
+return {
+  "zbirenbaum/copilot.lua",
+  opts = {
+    suggestion = {
+      keymap = {
+        accept = "<C-a>",
+        next = "<C-.>",
+        prev = "<C-,>",
+        dismiss = "<C/>",
+      },
+    },
+  },
+}
